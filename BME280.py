9d88355fbaa10809e1d1346c1d27020313271097
Pavlo Nikulin <pavlo.nikulin@obi.de>
2025-04-10 22:50:08 +0200

    refactor dashboard

--- BME280.py
+++ BME280.py
@@ -1,25 +1,32 @@
-# IIO_PATH = "/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0077/iio:device0/"
-IIO_PATH = "/Users/PNikulin/Berufsschule/Mittelstufe/EvP/bme280/inputs/"
+FALLBACK_IIO_PATH = "/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0077/iio:device0/"
 HUMIDITY_INPUT_FILE = "in_humidityrelative_input"
 PRESSURE_INPUT_FILE = "in_pressure_input"
 TEMPERATURE_INPUT_FILE = "in_temp_input"
 
+
 class BME280:
+
+    def __init__(self, iio_path):
+        self.iio_path = iio_path or FALLBACK_IIO_PATH
+
+
     def get_temperature(self):
         return self.__string_input_to_float(
-            self.__read_file(f"{IIO_PATH}{TEMPERATURE_INPUT_FILE}")
+            self.__read_file(f"{self.iio_path}{TEMPERATURE_INPUT_FILE}")
         )
+
 
     def get_humidity(self):
         return self.__string_input_to_float(
-            self.__read_file(f"{IIO_PATH}{HUMIDITY_INPUT_FILE}")
+            self.__read_file(f"{self.iio_path}{HUMIDITY_INPUT_FILE}")
         )
 
 
     def get_pressure(self):
         return self.__string_input_to_float(
-            self.__read_file(f"{IIO_PATH}{PRESSURE_INPUT_FILE}")
+            self.__read_file(f"{self.iio_path}{PRESSURE_INPUT_FILE}")
         )
+
 
     @staticmethod
     def __string_input_to_float(string_input):
@@ -28,12 +35,5 @@ class BME280:
 
     @staticmethod
     def __read_file(filename):
-        try:
-            with open(filename, 'r') as f:
-                return f.read().strip()
-        except FileNotFoundError:
-            print(f"File {filename} not found")
-        except PermissionError:
-            print(f"Permission denied when accessing {filename}")
-
-        return None
+        with open(filename, 'r') as f:
+            return f.read().strip()
