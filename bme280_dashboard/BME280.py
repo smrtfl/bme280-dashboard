349721ccb223f024b8ff6d42fecc24d057e4943e
Pavlo Nikulin <pavlo.nikulin@obi.de>
2025-05-07 13:14:53 +0200

    remove watchdog

--- bme280_dashboard/BME280.py
+++ bme280_dashboard/BME280.py
@@ -1,21 +1,12 @@
-from watchdog.events import DirModifiedEvent, FileModifiedEvent, FileSystemEventHandler
-from watchdog.observers import Observer
+import threading
+import time
 
 FALLBACK_IIO_PATH = "/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0077/iio:device0/"
 HUMIDITY_INPUT_FILE = "in_humidityrelative_input"
 PRESSURE_INPUT_FILE = "in_pressure_input"
 TEMPERATURE_INPUT_FILE = "in_temp_input"
 
-
-class BME280InputChangeEventHandler(FileSystemEventHandler):
-
-    def __init__(self, bme280_instance):
-        self.bme280_instance = bme280_instance
-
-
-    def on_modified(self, event: DirModifiedEvent | FileModifiedEvent):
-        if str(event.src_path).rsplit('/', 1)[-1] in [sensor.input_file for sensor in self.bme280_instance.sensors.values()]:
-            self.bme280_instance.update_file_readings(event.src_path)
+UPDATE_INTERVAL = 1.0
 
 
 class BME280Sensor:
@@ -29,8 +20,9 @@ class BME280:
 
     def __init__(self, iio_path=None):
         self.__iio_path = iio_path or FALLBACK_IIO_PATH
-        self.__event_handler = BME280InputChangeEventHandler(self)
-        self.__observer = Observer()
+        self.__update_interval = UPDATE_INTERVAL
+        self.__stop_event = threading.Event()
+        self.__thread = threading.Thread(target=self.__update_loop, daemon=True)
 
         self.sensors = {
             "temperature": BME280Sensor(None, TEMPERATURE_INPUT_FILE),
@@ -38,21 +30,20 @@ class BME280:
             "pressure": BME280Sensor(None, PRESSURE_INPUT_FILE),
         }
 
-        self.__observer.schedule(self.__event_handler, self.__iio_path, recursive=False)
-
         for sensor_name in self.sensors.keys():
             self.__update_sensor_value(sensor_name)
 
-        self.__observer.start()
+
+    def stop(self):
+            self.__stop_event.set()
+            self.__thread.join()
 
 
-    def get_sensor_filepath(self, sensor):
-        return f"{self.__iio_path}{sensor.input_file}"
-
-
-    def update_file_readings(self, filename):
-        sensor_name = next((name for name, sensor in self.sensors.items() if sensor.input_file == filename.rsplit('/', 1)[-1]), None)
-        self.__update_sensor_value(sensor_name)
+    def __update_loop(self):
+        while not self.__stop_event.is_set():
+            for sensor_name in self.sensors:
+                self.__update_sensor_value(sensor_name)
+            time.sleep(self.__update_interval)
 
 
     def __update_sensor_value(self, sensor_name):
