bf7ae1af07b37ebf10f6d7d34850fb36e07b7cd9
Pavlo Nikulin <pavlo.nikulin@obi.de>
2025-06-10 15:23:58 +0200

    add missing files

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+from getopt import getopt
+import curses
+from BME280 import BME280
+from Dashboard import Dashboard
+
+
+def parse_path(path):
+    return path if path.endswith('/') else path + '/'
+
+
+def get_iio_path():
+    iio_path_option = "iio-path"
+
+    try:
+        opts, args = getopt(sys.argv[1:], '', [f"{iio_path_option}="])
+    except:
+        print("Usage: ./bme280_dashboard --iio-path path/to/iio/device")
+        sys.exit(2)
+
+    if args:
+        print("Error: Unexpected arguments provided.")
+        print("Usage: ./bme280_dashboard --iio-path path/to/iio/device")
+        sys.exit(2)
+
+    iio_path = next((value for option, value in opts if option == "--iio-path"), None)
+
+    return parse_path(iio_path) if iio_path else None
+
+
+if __name__ == "__main__":
+    bme280 = BME280(get_iio_path())
+    curses.wrapper(lambda stdscr: Dashboard(stdscr, bme280))
